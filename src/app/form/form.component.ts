9f3fd336a88c9969ea2d9e0204fbab892ad5699b
Umm-e-Hani18 <hayaa.official10@gmail.com>
2024-05-02 04:02:46 -0700

    matchpasswordValidation

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -10,7 +10,7 @@ import { FormControl, FormGroup, Validators,ReactiveFormsModule, AbstractControl
   templateUrl: './form.component.html',
   styleUrl: './form.component.css'
 })
-export class FormComponent {
+export class FormComponent  {
    integerRegex = /^\d+$/;
    emailRegex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
 
@@ -19,10 +19,10 @@ export class FormComponent {
     Fname : new FormControl ('',[Validators.required, Validators.maxLength(32)]),
     email : new FormControl ('',[Validators.required, Validators.maxLength(32),Validators.pattern(this.emailRegex)]),
     Password : new FormControl ('',[Validators.required, Validators.maxLength(32),this.passwordValidator()]),
-    confirm : new FormControl ('',[Validators.required, Validators.maxLength(32),this.passwordValidator()]),
+    confirm : new FormControl ('',[Validators.required, Validators.maxLength(32)])
 
 
-  })
+  },{ validators: this.passwordMatchValidator() });
   passwordValidator(): any {
     return (control: AbstractControl): { [key: string]: boolean } | null => {
       const value: string = control.value || '';
@@ -33,6 +33,19 @@ export class FormComponent {
       const valid = hasNumber && hasUpper && hasLower && value.length <= 8;
 
       return valid ? null : { invalidPassword: true };
+    };
+  }
+
+  passwordMatchValidator(): any {
+    return (signupForm: FormGroup): { [key: string]: boolean } | null => {
+      const password = signupForm.get('Password')?.value;
+      const confirmPassword = signupForm.get('confirm')?.value;
+      if (password !== confirmPassword) {
+        signupForm.get('confirm')?.setErrors({ mismatch: true });
+        return { mismatch: true };
+      } else {
+        return null;
+      }
     };
   }
 
